5cf275f61f8743c7e0f75b92d79e9b0dd1b7d06b
Duvan Gomez <gomez.duvan@outlook.com>
2021-12-02 21:11:07 -0500

    generación de la palabra de manera aleatoria

---
+++ Palindromos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Palindromos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            Random r = new Random();
+
+            int b = 65;
+            string palabra = "";
+
+            for (int count = 0; count < 10; count++)
+            {
+                palabra += (char)(64+r.Next(0,26));
+                b++;
+            }
+
+            Console.WriteLine($"la palabra es: {palabra}");
+
+            Console.ReadKey();
+        }
+    }
+}
